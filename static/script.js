e1767d14acb0e2e40b8d20e0a96450b1da3a649d
Prins <prinsbhuva2004@gmail.com>
2024-03-30 15:28:08 +0530

    improved footer

--- static/script.js
+++ static/script.js
@@ -19,3 +19,21 @@ let slideInterval = setInterval(() => {
 
 showSlides();
 
+function adjustFooterPosition() {
+    const body = document.body;
+    const html = document.documentElement;
+    const windowHeight = window.innerHeight;
+    const bodyHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+    const footer = document.querySelector('footer');
+    if (windowHeight >= bodyHeight) {
+        // If window height is greater than or equal to body height, position footer at the bottom of the viewport
+        footer.style.position = 'fixed';
+        footer.style.bottom = '0';
+    } else {
+        // If body height is greater than window height, position footer at the bottom of the content
+        footer.style.position = 'static';
+    }
+}
+
+window.addEventListener('load', adjustFooterPosition);
+window.addEventListener('resize', adjustFooterPosition);
